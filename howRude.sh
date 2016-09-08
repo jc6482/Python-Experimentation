ce696573ade0193c15a5b6b24b62fb706df2c247
Juan Carlos Ramirez <jc_ramirez@baylor.edu>
2016-09-08 15:56:27 -0500

    Made it usable on most linux

--- howRude.sh
+++ howRude.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 user=$1
 terminal=$2
-while [[ 1 ]]; do echo "$(python spiral.py -width 50)" | write $user $terminal; done
+while [ 1 ]; do echo "$(python spiral.py -width 50)" | write $user $terminal; done
