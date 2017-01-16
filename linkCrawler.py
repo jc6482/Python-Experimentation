09f51187e29144ae8f196e367ecd9697afcb809b
Juan Carlos Ramirez <jc_ramirez@baylor.edu>
2017-01-15 19:41:50 -0600

    Web page crawler that provides a list of outside sources with the src tag. Needs to be updated to include href tag.

---
+++ linkCrawler.py
@@ -0,0 +1,22 @@
+#Juan Carlos Ramirez
+#Crawls a web page for a list of all links referenced in the page
+
+import argparse, re
+from urllib2 import Request, urlopen, URLError
+from pprint import pprint
+parser = argparse.ArgumentParser(description="Crawls a web page for a list of all links referenced in the page")
+parser.add_argument("-host", help="Specify a host", default="www.google.com")
+
+args = parser.parse_args()
+
+
+def remove_html_markup(s):
+    out = re.findall("src=\"(.*?)\"",s)
+    return out
+
+try:
+    response = urlopen(Request(args.host))
+
+    pprint(remove_html_markup(response.read()))
+except URLError, e:
+    print "An error has occured, check the URL?", e
